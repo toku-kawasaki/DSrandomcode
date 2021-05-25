7e2bdaa6af2e542bb2075f90b9694856868b4a5c
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-05-25 19:27:04 +0900

    Separate classfile

--- DSrandom.py
+++ DSrandom.py
@@ -1,34 +1,7 @@
-import random as ra
 import pyperclip as pc
+import classes as cl
 
-class randode:
-    a = "0x2000000"
-    b = "0x23FFFFF"
-    def rand8bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
-        adhani = ra.randint(i_a,i_b)
-        cdhani = ra.randint(0,255)
-        adh = hex(adhani)
-        cdh = hex(cdhani)
-        return adh[2:] +" "+cdh[2:].zfill(8)
-
-    def rand16bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
-        adhani = ra.randint(i_a,i_b)
-        cdhani = ra.randint(0,65535)
-        adh = hex(adhani)
-        cdh = hex(cdhani)
-        return adh[2:] +" "+cdh[2:].zfill(8)
-
-    def rand32bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
-        adhani = ra.randint(i_a,i_b)
-        cdhani = ra.randint(0,4294967295)
-        adh = hex(adhani)
-        cdh = hex(cdhani)
-        return adh[2:] +" "+cdh[2:].zfill(8)
-
-DSc = randode()
+DSc = cl.randode()
 lis = []
 inp_bit = input("コードのビットを入力してください(2=8bit,1=16bit,0=32bit)：")
 inp_cnt = int(input("コードの範囲を入力してください："))
