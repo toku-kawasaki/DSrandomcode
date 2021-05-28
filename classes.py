56391207ce9e2f751cc9df9ab7f03a2bda62a8df
toku-kawasaki <rinkodatoukyu@gmail.com>
2021-05-28 18:58:10 +0900

    add randchange

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 import random as ra
 
 class randkotei:
-    a = "0x2000000"
-    b = "0x23FFFFF"
+    st = "0x2000000"
+    en = "0x23FFFFF"
     def rand8bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
         adhani = ra.randint(i_a,i_b)
         cdhani = ra.randint(0,255)
         adh = hex(adhani)
@@ -12,7 +12,7 @@ class randkotei:
         return adh[2:] +" "+cdh[2:].zfill(8)
 
     def rand16bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
         adhani = ra.randint(i_a,i_b)
         cdhani = ra.randint(0,65535)
         adh = hex(adhani)
@@ -20,7 +20,37 @@ class randkotei:
         return adh[2:] +" "+cdh[2:].zfill(8)
 
     def rand32bit(self):
-        i_a,i_b = int(self.a,base=16),int(self.b,base=16)
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
+        adhani = ra.randint(i_a,i_b)
+        cdhani = ra.randint(0,4294967295)
+        adh = hex(adhani)
+        cdh = hex(cdhani)
+        return adh[2:] +" "+cdh[2:].zfill(8)
+
+class randchange:
+
+    def __init__(self,st,en):
+        self.st = st
+        self.en = en
+
+    def rand8bit(self):
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
+        adhani = ra.randint(i_a,i_b)
+        cdhani = ra.randint(0,255)
+        adh = hex(adhani)
+        cdh = hex(cdhani)
+        return adh[2:] +" "+cdh[2:].zfill(8)
+
+    def rand16bit(self):
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
+        adhani = ra.randint(i_a,i_b)
+        cdhani = ra.randint(0,65535)
+        adh = hex(adhani)
+        cdh = hex(cdhani)
+        return adh[2:] +" "+cdh[2:].zfill(8)
+
+    def rand32bit(self):
+        i_a,i_b = int(self.st,base=16),int(self.en,base=16)
         adhani = ra.randint(i_a,i_b)
         cdhani = ra.randint(0,4294967295)
         adh = hex(adhani)
